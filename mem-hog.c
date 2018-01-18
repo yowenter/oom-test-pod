5ac2aebdf27542bb2be49925e2f78e5a6ef5dfb0
TaoGe <wenter.wu@gmail.com>
2018-01-18 11:02:01 +0800

    Create mem-hog.c

---
+++ mem-hog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define KB (1024)
+#define MB (1024 * KB)
+#define GB (1024* MB)
+
+
+
+int main(int argc, char *argv[]){
+
+  printf("Evil Allocating Memory...\n");
+
+  printf("Will Allocate Memory: %s*100MB \n",argv[1]);
+
+  int mem;
+  mem = atoi(argv[1]);
+  int i=0;
+  char *p ;
+
+  while(i<mem){
+    p = (char * )malloc(100*MB);
+    memset(p, 0, 100*MB);
+    printf("Allocating 100 MB OK. Sleep 1 sec\n");
+    sleep(1);
+    i++;
+
+}
+
+
+  printf("%d*100MB memory allocated..go to sleep for 1000 sec.\n",3);
+
+  sleep(1000);
+
+
+
+}
